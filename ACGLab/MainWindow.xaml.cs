dd420bfdcc4fa80cd12a0582ef0c81fff4ce42ec
feoNikpl <feo.pl@mail.ru>
2021-09-06 23:49:25 +0300

    improve render

--- ACGLab/MainWindow.xaml.cs
+++ ACGLab/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Drawing;
 using Color = System.Drawing.Color;
 using System.Numerics;
+using ACGLab.View;
 
 namespace ACGLab
 {
@@ -21,17 +22,18 @@ namespace ACGLab
         float Zoom = 1.0f;
         int X = 0, Y = 0, Z = 0;
         float Ox = 0, Oy = 0, Oz = 0;
-        float CameraSpeed = 0.05f;
+        float CameraSpeed = 5.0f;
         Vector3 CamPos = new Vector3(1.0f, 1.0f, 1.0f), CamTarget = new Vector3(0.0f, 0.0f, 0.0f), CamUp = new Vector3(0.0f, 1.0f, 0.0f);
         Vector3 SpeedVecOx = new Vector3(1f, 0f, 0f);
         Vector3 SpeedVecOy = new Vector3(0f, 1f, 0f);
         Vector3 SpeedVecOz = new Vector3(0f, 0f, 1f);
+        BitmapDrawing drawing = new BitmapDrawing();
 
-        Bitmap bitmap;
         public MainWindow()
         {
             InitializeComponent();
-            bitmap = new Bitmap((int)Width, (int)Height);
+            drawing.bitmap = new WriteableBitmap((int)Width, (int)Height, 96, 96, PixelFormats.Bgr32, null);
+            DataContext = drawing;
             Draw();
 
         }
@@ -39,74 +41,73 @@ namespace ACGLab
 
         private void Draw()
         {
-            bitmap = ClearBitmap(bitmap, Color.White);
-            double[,] a = Transformation.Transformation.GetTransformationMatrix(CamPos, CamTarget, CamUp, Width, Height, 0.0f, 1.0f, Zoom, X, Y, Z, Ox, Oy, Oz);
-            foreach (Polygon p in drawingObject.Instance)
+            double dx, dy, x1, x2, y1, y2;
+            int L;
+            double[,] transformMatrix = Transformation.Transformation.GetTransformationMatrix(CamPos, CamTarget, CamUp, Width, Height, 0.0f, 1.0f, Zoom, X, Y, Z, Ox, Oy, Oz);
+            try
             {
-                bitmap = DrawPolygon(bitmap, p, a);
-            }
-            Canvas.Source = BitmapToImageSource(bitmap);
-
-
-        }
-        private Bitmap ClearBitmap(Bitmap bitmap, Color color)
-        {
-            for (int i = 0; i < bitmap.Width; i++)
-                for (int j = 0; j < bitmap.Height; j++)
-                    bitmap.SetPixel(i, j, color);
-            return bitmap;
-        }
-        private BitmapImage BitmapToImageSource(Bitmap bitmap)
-        {
-            using (MemoryStream memory = new MemoryStream())
-            {
-                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-                memory.Position = 0;
-                BitmapImage bitmapimage = new BitmapImage();
-                bitmapimage.BeginInit();
-                bitmapimage.StreamSource = memory;
-                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapimage.EndInit();
-
-                return bitmapimage;
-            }
-        }
-
-        public Bitmap DrawLine(Bitmap bitmap, double x1, double y1, double x2, double y2)
-        {
-            int L = (int)Math.Max(Math.Abs(x2 - x1), Math.Abs(y2 - y1));
-            double dx = (x2 - x1) / L;
-            double dy = (y2 - y1) / L;
-            for (int k = 0; k <= L; k++)
-            {
-                if (x1 > 0 && y1 > 0 && x1 < Width && y1 < Height)
-                    bitmap.SetPixel((int)x1, (int)y1, Color.Black);
-                x1 += dx;
-                y1 += dy;
-            }
-            if (x2 > 0 && y2 > 0 && x2 < Width && y2 < Height)
-                bitmap.SetPixel((int)x2, (int)y2, Color.Black);
-            return bitmap;
-        }
-
-        public Bitmap DrawPolygon(Bitmap bitmap, Polygon polygon, double[,] transformMatrix)
-        {
-            double[] point1, point2 = new double[4];
-            for (int i = 0; i < polygon.Vertices.Count; i++)
-            {
-                if (i == polygon.Vertices.Count - 1)
+                // Reserve the back buffer for updates.
+                drawing.bitmap.Lock();
+                unsafe
                 {
-                    point1 = MatrixMath.Multiplication(transformMatrix, polygon.Vertices[0].ToArray());
+                    int backColor = Color.White.ToArgb();
+                    for(int i = 0; i< drawing.bitmap.PixelWidth; i++)
+                        for (int j = 0; j < drawing.bitmap.PixelHeight; j++)
+                        {
+                            IntPtr pBackBuffer = drawing.bitmap.BackBuffer + (int)j * drawing.bitmap.BackBufferStride + (int)i * 4;
+                            *((int*)pBackBuffer) = backColor;
+                        }
+                            
+                    // Compute the pixel's color.
+                    int color_data = Color.Black.ToArgb();
+                    foreach (Polygon polygon in drawingObject.Instance)
+                    {
+                        double[] point1, point2 = new double[4];
+                        for (int i = 0; i < polygon.Vertices.Count; i++)
+                        {
+                            if (i == polygon.Vertices.Count - 1)
+                            {
+                                point1 = MatrixMath.Multiplication(transformMatrix, polygon.Vertices[0].ToArray());
+                            }
+                            else
+                            {
+                                point1 = MatrixMath.Multiplication(transformMatrix, polygon.Vertices[i].ToArray());
+                                point2 = MatrixMath.Multiplication(transformMatrix, polygon.Vertices[i + 1].ToArray());
+                            }
+                            x1 = point1[0];
+                            y1 = point1[1];
+                            x2 = point2[0];
+                            y2 = point2[1];
+                            L = (int)Math.Max(Math.Abs(x2 - x1), Math.Abs(y2 - y1));
+                            dx = (x2 - x1) / L;
+                            dy = (y2 - y1) / L;
+                            for (int k = 0; k <= L; k++)
+                            {
+                                if (x1 > 0 && y1 > 0 && x1 < Width && y1 < Height)
+                                {
+                                    IntPtr pBackBuffer = drawing.bitmap.BackBuffer + (int)y1 * drawing.bitmap.BackBufferStride + (int)x1 * 4;
+                                    *((int*)pBackBuffer) = color_data;
+                                }
+                                x1 += dx;
+                                y1 += dy;
+                            }
+                            if (x2 > 0 && y2 > 0 && x2 < Width && y2 < Height)
+                            {
+                                IntPtr pBackBuffer = drawing.bitmap.BackBuffer + (int)y2 * drawing.bitmap.BackBufferStride + (int)x2 * 4;
+                                *((int*)pBackBuffer) = color_data;
+                            }
+                        }
+                    }
+                    drawing.bitmap.AddDirtyRect(new Int32Rect(0, 0, drawing.bitmap.PixelWidth, drawing.bitmap.PixelHeight));
                 }
-                else
-                {
-                    point1 = MatrixMath.Multiplication(transformMatrix, polygon.Vertices[i].ToArray());
-                    point2 = MatrixMath.Multiplication(transformMatrix, polygon.Vertices[i + 1].ToArray());
-                }
-                DrawLine(bitmap, point1[0], point1[1], point2[0], point2[1]);
             }
-            return bitmap;
+            finally
+            {
+                drawing.bitmap.Unlock();
+            }
+
         }
+
 
         private void Canvas_MouseWheel(object sender, MouseWheelEventArgs e)
         {
---
+++ ACGLab/View/BitmapDrawing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using ACGLab.Model;
+
+namespace ACGLab.View
+{
+    public class BitmapDrawing
+    {
+        public WriteableBitmap bitmap { get; set; }
+    }
+}
